ecc2b9d238f0210ebc53fe678fb2082c2832a79f
Luiz <elisa.nasc.mot@gmail.com>
2022-03-22 02:10:58 -0300

    atualizando

---
+++ par.js
@@ -0,0 +1,11 @@
+var aluno = 14;
+for (let x = 0; x <= aluno; x++) {
+    if (x == 0) {
+        console.log("numero zero");
+    } else if (x % 2 == 0) {
+        console.log("numero é par: " + x);
+    } else {
+
+        console.log("numero é impar: " + x);
+    }
+}
